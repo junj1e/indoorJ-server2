9382587da3045d38c8eb11dcc9807fc367e8743f
junjie <cnhjj2008@gmail.com>
2017-05-31 23:06:06 +0800

    dddd

--- indoorJ-server2.py
+++ indoorJ-server2.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import os
 
 app = Flask(__name__)
 #app.run(host='0.0.0.0')
@@ -7,9 +8,22 @@ app.debug = True
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
 @app.route('/hello')
 def hello():
     return 'TuT_'
+
+@app.route('/file', methods=['POST', 'GET'])
+def update_file():
+    if request.method == 'POST':
+        # Get file object from field of file
+        f = request.files['userfile']
+        f.save(os.path.join('C:\\Users\\Nova\\Desktop', f.filename))
+        # Get str object from field of text
+        s = request.form['name']
+        with open('/home/ping/Documents/test.txt', 'a') as f:
+            f.write(s)
+    return 'Done!'
 
 if __name__ == '__main__':
     app.run()
