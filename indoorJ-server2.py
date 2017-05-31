b6406c2b675041ee84d27924f99c51adfdbd90d1
junjieHuang <junjihua@cisco.com>
2017-05-31 17:48:14 +0800

    first try

--- indoorJ-server2.py
+++ indoorJ-server2.py
@@ -2,11 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 #app.run(host='0.0.0.0')
+app.debug = True
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
-
+@app.route('/hello')
+def hello():
+    return 'TuT_'
 
 if __name__ == '__main__':
     app.run()
